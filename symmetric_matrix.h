f963aefc7f09786ea6561a1068371f66c3f1a8b2
Wk-P <72795540+Wk-P@users.noreply.github.com>
2022-07-14 11:05:54 +0900

    Add files via upload

---
+++ symmetric_matrix.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+// 对称矩阵 symmetric_matrix
+
+/*  关于 [0][0] 至 [n][n] 对角线对称 保留左下角数据 
+	从而寻找a[i][j] 所对应的线性存储的 a[k]值
+*/
+
+
+int _index(int (*array)[], int fst_s, int sec_s, int fst_index, int sec_index) {
+	/*
+	*	--param--
+		fst_s: 第一维数组长度（行数）
+		sec_s: 第二维数组长度（列数）
+		fst_index: 第一维数组下标
+		sec_index: 第二维数组下标
+	*/
+	return ((fst_index - 1) * fst_index)/2 + sec_index;
+}
