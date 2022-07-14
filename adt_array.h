8932a701b5c20f36aace8cee1bd062cabb8a24f9
Wk-P <72795540+Wk-P@users.noreply.github.com>
2022-07-14 10:54:03 +0900

    Add files via upload

---
+++ adt_array.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+// 自定义一维数组
+
+typedef struct Array {
+	int* data;
+	int size;
+} Array;
+
+bool init_array(Array* array, int size) {
+	if (!size || !array) return false;
+	array->data = new int[size];
+	array->data = { 0 };
+	array->size = size;
+	return true;
+}
+
+bool destroy_array(Array* array) {
+	if (!array) return false;
+	delete array->data;
+	delete array;
+	return true;
+}
+
+bool _assign(Array* array, int index, int data) {
+	array->data[index] = data;
+}
+
+int get_data(Array* array, int index) {
+	return array->data[index];
+}
