df41f111acd59e4c4d2f3e2142561b5d4adad4ec
Wk-P <72795540+Wk-P@users.noreply.github.com>
2022-08-28 01:54:43 +0900

    Update 选择排序.cpp

--- sort algorithm/选择排序.cpp
+++ sort algorithm/选择排序.cpp
@@ -1,6 +1,6 @@
 ﻿#include "array.h"
 
-void selection_sort(int *arr, int size) {
+void selection_sort1(int *arr, int size) {
 	for (int i = 0; i < size; i++) {
 		int min = i;
 		int j = i + 1;
@@ -15,6 +15,25 @@ void selection_sort(int *arr, int size) {
 		int temp = arr[i];
 		arr[i] = arr[min];
 		arr[min] = temp;
+	}
+}
+
+void selection_sort2(int *arr, int size) {
+	for (int i = 0; i < size; i++) {
+		int min = i;
+		for (int j = i + 1; j < size; j++) {
+			if (arr[j] < arr[min]) {
+				min = j;
+			}
+		}
+		// 判定是否需要交换
+		if (min == i) {
+			continue;
+		} else {
+			int temp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = temp;
+		}
 	}
 }
 
